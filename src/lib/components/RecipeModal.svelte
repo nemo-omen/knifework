f4ef0e8593f6cb8b2e78910320dcc5f59a68855f
trainingmontage <nemo.omen@gmail.com>
2021-12-08 09:31:21 -0600

    Add icon buttons to modal

--- src/lib/components/RecipeModal.svelte
+++ src/lib/components/RecipeModal.svelte
@@ -8,6 +8,7 @@
   import { fade } from 'svelte/transition';
   import { quintIn, quintOut } from 'svelte/easing';
   import { createEventDispatcher } from 'svelte';
+  import Icon from './Icon.svelte';
   const dispatch = createEventDispatcher();
   export let recipe;
   console.log(recipe);
@@ -22,9 +23,22 @@
   in:fade={{ duration: 400, easing: quintIn }}
   out:fade={{ duration: 400, easing: quintOut }}
 >
-  <div class="recipe-info">
+  <div class="modal-head">
     <h2>{recipe.name}</h2>
-    <img class="recipe-image" src={recipe.photourl} width="800" height="450" alt={recipe.name} />
+    <div class="modal-control">
+      <button class="icon-button modal-button" on:click={() => dispatch('edit')}>
+        <Icon name="edit" />
+      </button>
+      <button class="icon-button modal-button" on:click={() => dispatch('close')}>
+        <Icon name="close" />
+      </button>
+    </div>
+  </div>
+  <div class="recipe-info">
+    {#if recipe.image}
+      <img class="recipe-image" src={recipe.photourl} width="800" height="450" alt={recipe.name} />
+    {/if}
+
     <div class="recipe-prep">
       <h3>Serves</h3>
       <p>{recipe.servings}</p>
@@ -36,16 +50,9 @@
       <ul class="recipe-list">
         {#each recipe.ingredients as ingredient}
           <li>{ingredient.Ingredient.name}</li>
-          <!-- <li>{JSON.stringify(ingredient.Ingredient)}</li> -->
         {/each}
       </ul>
     </div>
-    <!-- {#if recipe.imageUrl} -->
-    <!-- {/if} -->
-  </div>
-  <div class="modal-control">
-    <button on:click={() => dispatch('save')} class="ui-button">Edit</button>
-    <button on:click={() => dispatch('close')} class="ui-button">Close</button>
   </div>
 </div>
 
@@ -53,43 +60,37 @@
   .modal {
     display: flex;
     flex-direction: column;
-    /* gap: 2rem; */
+    gap: 2rem;
     position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: var(--gray-9);
+    top: 1rem;
+    left: 10vw;
+    right: 10vw;
+    bottom: 1rem;
+    background-color: var(--gray-8);
+    border-radius: 1rem;
+    box-shadow: var(--shadow-4);
     padding: 1rem;
+  }
+
+  .modal-head {
+    display: flex;
+    justify-content: space-between;
   }
 
   .modal-control {
     display: flex;
-    justify-content: center;
-    gap: 2rem;
+    gap: 1rem;
   }
 
-  .ui-button {
-    border: none;
-    background-color: var(--accent);
-    font-size: 1.25rem;
-    font-weight: 600;
-    letter-spacing: 0.125em;
-    text-transform: uppercase;
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    cursor: pointer;
-    transition: all 0.3s ease-out;
-    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
-  }
-
-  .ui-button:hover {
-    background-color: var(--accent-secondary);
-    box-shadow: 3px 3px 2px rgba(0, 0, 0, 0.4);
-  }
-
-  .ui-button:active {
-    background-color: var(--light);
+  .modal-button {
+    min-height: unset;
+    max-height: unset;
+    padding: 0;
+    padding: 0.25rem;
+    border-radius: var(--radius-2);
+    border: 1px solid var(--gray-7);
+    /* box-shadow: var(--shadow-2); */
+    /* background-color: var(--gray-8); */
   }
 
   .recipe-info {
@@ -100,8 +101,8 @@
     grid-template-columns: repeat(2, 1fr);
     align-items: end; */
     /* grid-gap: 10vw; */
-    max-width: 60%;
-    margin: 0 auto;
+    /* max-width: 60%; */
+    /* margin: 0 auto; */
   }
 
   .recipe-list {
