bf78b16922bf818b8a1a7a4de0ed14cdad5492cb
nemo-omen <nemo.omen@gmail.com>
2021-12-09 13:32:49 -0600

    Basic layout

--- src/lib/pages/PostRecipe.svelte
+++ src/lib/pages/PostRecipe.svelte
@@ -5,6 +5,8 @@
 
   let name;
   $: servings = 0;
+  $: cooktime = 0;
+  $: preptime = 0;
 
   function incrementServings() {
     servings++;
@@ -14,6 +16,30 @@
     if (servings > 0) {
       servings--;
     }
+  }
+
+  function incrementCooktime() {
+    cooktime++;
+  }
+
+  function decrementCooktime() {
+    if (cooktime > 0) {
+      cooktime--;
+    }
+  }
+
+  function incrementPreptime() {
+    preptime++;
+  }
+
+  function decrementPreptime() {
+    if (preptime > 0) {
+      preptime--;
+    }
+  }
+
+  function handleImageUpload() {
+    console.log('TODO!!!');
   }
 </script>
 
@@ -25,34 +51,72 @@
           <label for="name">Name</label>
           <input type="text" name="name" id="name" bind:value={name} />
         </div>
-        <div class="form-image">
-          <button class="icon-button no-bg huge">
-            <Icon name="image" />
-          </button>
+
+        <div class="input-group third">
+          <span>Servings</span>
+          <div class="button-group">
+            <button class="icon-button no-bg" on:click={decrementServings}>
+              <Icon name="subtract" />
+            </button>
+            <button class="icon-button no-bg" on:click={incrementServings}>
+              <Icon name="add2" />
+            </button>
+          </div>
+          <span class="servings">{servings}</span>
+        </div>
+
+        <div class="input-group third">
+          <span>Prep Time</span>
+          <div class="button-group">
+            <button class="icon-button no-bg" on:click={decrementPreptime}>
+              <Icon name="subtract" />
+            </button>
+            <button class="icon-button no-bg" on:click={incrementPreptime}>
+              <Icon name="add2" />
+            </button>
+          </div>
+          <span class="servings">{preptime} min</span>
+        </div>
+
+        <div class="input-group third">
+          <span>Cook Time</span>
+          <div class="button-group">
+            <button class="icon-button no-bg" on:click={decrementCooktime}>
+              <Icon name="subtract" />
+            </button>
+            <button class="icon-button no-bg" on:click={incrementCooktime}>
+              <Icon name="add2" />
+            </button>
+          </div>
+          <span class="servings">{cooktime} min</span>
         </div>
       </div>
 
-      <div class="input-group third">
-        <span>Servings</span>
-        <div class="button-group">
-          <button class="icon-button no-bg" on:click={decrementServings}>
-            <Icon name="subtract" />
-          </button>
-          <button class="icon-button no-bg" on:click={incrementServings}>
-            <Icon name="add2" />
-          </button>
-        </div>
-        <span class="servings">{servings}</span>
+      <div class="form-image" on:click={handleImageUpload}>
+        <button class="icon-button no-bg huge">
+          <Icon name="image" />
+        </button>
+      </div>
+
+      <div class="form-group instruction">
+        <label for="instructions">Instructions</label>
+        <textarea name="instructions" id="instructions" cols="30" rows="10" />
+      </div>
+
+      <div class="form-group notes">
+        <label for="instructions">Notes</label>
+        <textarea name="instructions" id="instructions" cols="30" rows="10" />
       </div>
     </div>
+    <!--/form-->
   </div>
 </section>
 
 <style>
   .recipe-form {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 3rem;
     position: absolute;
     top: 1rem;
     left: 15vw;
@@ -63,10 +127,9 @@
     padding: 2rem 2.25rem;
   }
   .form-group {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    align-items: start;
-    gap: 3rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
   }
 
   .form-image {
@@ -76,28 +139,26 @@
     align-items: center;
     border: 1px solid var(--gray-6);
     border-radius: var(--radius-2);
+    cursor: pointer;
+    transition: all 0.3s ease-out;
+  }
+
+  .form-image:hover {
+    border-color: var(--gray-8);
+    color: var(--gray-5);
+    background-color: var(--gray-9);
   }
 
   .input-group {
     display: flex;
-    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     gap: 0.25rem;
-  }
-
-  .input-group input {
-    /* order: 1; */
+    font-size: 1.25rem;
   }
 
   .input-group label {
     padding-inline: 0.5rem;
-    /* order: 2; */
-  }
-
-  .third {
-    display: grid;
-    grid-template-columns: auto 1fr 10%;
-    align-items: center;
-    max-width: 50%;
   }
 
   .button-group {
@@ -105,7 +166,6 @@
     justify-self: end;
   }
   .servings {
-    font-size: 2rem;
     justify-self: end;
   }
 
